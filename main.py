e31cd3916aa21a13df1ff11f6cc05ac1c477893a
bascobane-15 <bascobane@gmail.com>
2026-02-15 16:44:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import streamlit as st
 import pandas as pd
-import base64
 
 st.set_page_config(page_title="BioTwin-Systems", layout="wide")
 # SOL PANEL (SIDEBAR) EKLEYELİM
- 
+ with st.sidebar:
+    import base64
 
-with open("anatomy.mp4", "rb") as f:
-    video_bytes = f.read()
-    video_base64 = base64.b64encode(video_bytes).decode()
+    with open("anatomy.mp4", "rb") as f:
+        video_bytes = f.read()
+        video_base64 = base64.b64encode(video_bytes).decode()
 
-st.markdown(f"""
-<video width="100%" autoplay loop muted playsinline>
-    <source src="data:video/mp4;base64,{video_base64}" type="video/mp4">
-</video>
-""", unsafe_allow_html=True)
+    st.markdown(f"""
+    <video width="100%" autoplay loop muted playsinline>
+        <source src="data:video/mp4;base64,{video_base64}" type="video/mp4">
+    </video>
+    """, unsafe_allow_html=True)
+
+    st.title("BioTwin Dashboard")
+
 
     st.title("BioTwin Dashboard")
     st.markdown("---")
@@ -317,6 +320,7 @@ with tabs[3]:
 
 st.divider()
 st.caption("BioTwin-Systems | Eğitim Amaçlı Dijital İkiz Modeli")
+
 
 
 

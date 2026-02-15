764d4a5a9761b566969995079a9f7a75be12c284
bascobane-15 <bascobane@gmail.com>
2026-02-15 16:37:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ import pandas as pd
 st.set_page_config(page_title="BioTwin-Systems", layout="wide")
 # SOL PANEL (SIDEBAR) EKLEYELİM
 with st.sidebar:
-    st.video("anatomy.mp4")
+    st.markdown("""
+<video width="100%" autoplay loop muted playsinline>
+    <source src="anatomy.mp4" type="video/mp4">
+</video>
+""", unsafe_allow_html=True)
 
     st.title("BioTwin Dashboard")
     st.markdown("---")
@@ -308,6 +312,7 @@ with tabs[3]:
 
 st.divider()
 st.caption("BioTwin-Systems | Eğitim Amaçlı Dijital İkiz Modeli")
+
 
 
 
